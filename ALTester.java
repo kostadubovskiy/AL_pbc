58c6c1e3e9a3d496614a5a779e7c8656c6123d04
phuang30 <phuang30@stuy.edu>
2021-12-13 13:03:16 -0500

    hi

--- ALTester.java
+++ ALTester.java
@@ -1,13 +1,22 @@
-public class AlTester {
+import java.util.ArrayList;
 
+public class ALTester {
+  ArrayList<Integer> _data = new ArrayList<Integer>(23);
+  int _size;
+   public ALTester () {
+     for (Object i : _data) {
+       i = Math.random() * 100;
+     }
+   }
 
-
-
-
-
-
-
-
-
+   public boolean isSorted () {
+     boolean sorted = true;
+     for (int i = 0; i < _data.size(); i++) {
+       if (_data.get(i) > _data.get(i+1)) {
+         sorted = false;
+       }
+     }
+     return sorted;
+   }
 
 }
